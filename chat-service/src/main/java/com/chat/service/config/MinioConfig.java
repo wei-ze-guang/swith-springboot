1813922b57f06e6e63367ca00b11b374e50eb6ee
weizeguang <705204898@qq.com>
2025-05-13 00:34:50 +0800

    睡觉提交

---
+++ chat-service/src/main/java/com/chat/service/config/MinioConfig.java
@@ -0,0 +1,31 @@
+package com.chat.service.config;
+
+import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MinioConfig {
+
+    @Value("${minio.endpoint}")
+    private  String  endpoint;
+
+    @Value("${minio.access-key}")
+    private  String accessKey;
+
+    @Value("${minio.secret-key}")
+    private  String secretKey;
+
+    @Value("${minio.bucket-name}")
+    private  String bucketName;
+
+    @Bean
+    public MinioClient minioClient() {
+        return MinioClient.builder()
+                .endpoint(endpoint)
+                .credentials(accessKey, secretKey)
+                .build();
+    }
+
+}
